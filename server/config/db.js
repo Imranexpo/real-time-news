01a4ac1cc5e979fdb839bd2f3f1ba1031ff2dee9
Mohammed Imran <imranexpo864@gmail.com>
2025-05-18 21:16:56 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✅ MongoDB connected successfully');
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+    console.log('MongoDB connected');
+  })
   } catch (err) {
     console.error('❌ MongoDB connection failed:', err.message);
     process.exit(1); // Exit process with failure
@@ -14,3 +13,5 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+
