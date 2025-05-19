88e64b0c5130d32eda1a6aaaab12346410e9da52
Mohammed Imran <imranexpo864@gmail.com>
2025-05-19 22:58:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,15 +3,24 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const cors = require('cors');
-const serverless = require('serverless-http');
-require('./scheduler/schedular')
+require('./scheduler/schedular');
+
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
 connectDB();
+
+// Routes
 app.use('/api', authRoutes);
 
-module.exports.handler = serverless(app);
+// Root route
+app.get("/", (req, res) => {
+  res.send("Real-time News API is running");
+});
+
+// Listen
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+app.listen(PORT, () => console.log(Server running on port ${PORT}));
